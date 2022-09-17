8ec625ace38c1d3149ac3252eecc00973a67c7bf
Abhineet Raj <82033840+abhineetraj1@users.noreply.github.com>
2022-09-17 14:37:46 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,33 @@
+# Hotel room booking system
+This system helps to book and reserve rooms in hotel.
+
+## Features :-
+*	Register rooms
+*	Clear all registerations in room
+*	View registered dates of booked rooms
+
+### Note:-
+This project is in developmental phase, it's not ready for production.
+
+## Installation
+
+1)	Download django
+```
+pip3 install django
+```
+
+## Execution
+
+*	Open the project folder.
+*	Open terminal in that folder using this commnand:-
+```
+python3 manage.py runserver
+```
+*	And then open web browser and go to -> http://localhost:5000/
+
+
+## Languages and tools used:
+<a href="https://www.djangoproject.com/" target="_blank" rel="noreferrer"> <img src="https://cdn.worldvectorlogo.com/logos/django.svg" alt="django" width="40" height="40"/> </a><a href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"/> </a><a href="https://www.python.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"/> </a> </p>
+
+## Authors
+[@abhineetraj1](http://github.com/abhineetraj1/)
---
+++ views/views.py
@@ -0,0 +1,64 @@
+import shutil
+import os
+from django.http import HttpResponse
+from django.shortcuts import render
+from datetime import *
+
+def Home(request):
+	return render (request, "index.html")
+
+def Room(request):
+	if (request.method == "POST"):
+		dt = list_days(request.POST.get("check_in"), request.POST.get("check_out"))
+		r=[]
+		for i in os.listdir("rooms"):
+			st=open("rooms/"+i+"/status.txt","r").read()
+			if (st == "clear"):
+				r.append(str(i))
+			else:
+				de = []
+				h= open("rooms/"+i+"/date.txt","r").read().split("\n")
+				for k in dt:
+					if (k not in h):
+						de.append(True)
+					else:
+						de.append(False)
+				if (False not in de):
+					r.append(str(i))
+		return render(request, "rooms.html",{"r":r,"message":"none"})
+	else:
+		return HttpResponse("<a href='/'><button>back</button></a>")
+
+def Book(request,room, in_d, out_d, details):
+	r, tr=[],""
+	for i in list_days(in_d,out_d):
+		tr = tr+i +"\n"
+	open("rooms/"+room+"/date.txt","a").write(tr)
+	open("rooms/"+room+"/status.txt","w").write("booked")
+	open("rooms/"+room+"/detail.txt","a").write(details+" "+in_d+","+out_d+"\n")
+	return HttpResponse("Booked")
+
+def list_days(a,b):
+	o = []
+	d = date(int(a.split("-")[0]), int(a.split("-")[1]), int(a.split("-")[2])) - date(int(b.split("-")[0]), int(b.split("-")[1]), int(b.split("-")[2]))
+	d=d.days-(2* d.days)
+	n = date(int(a.split("-")[0]), int(a.split("-")[1]), int(a.split("-")[2]))
+	for i in range(0, 22):
+		n = n+ timedelta(days=i)
+		o.append(str(n))
+	return o
+
+def list_booked(request):
+	g=[]
+	for i in os.listdir("rooms"):
+		if (open("rooms/"+i+"/status.txt","r").read() == "booked"):
+			g.append({"number":i,"dates":open("rooms/"+i+"/date.txt","r").read().replace("\n","<br>")})
+	return render(request, "pr.html", {"room":g})
+
+def deBook(request, room):
+	shutil.rmtree("rooms/"+room)
+	os.mkdir("rooms/"+room)
+	open("rooms/"+room+"/status.txt","a").write("clear")
+	open("rooms/"+room+"/detail.txt","a").write("")
+	open("rooms/"+room+"/date.txt","a").write("")
+	return HttpResponse("Debooked")
